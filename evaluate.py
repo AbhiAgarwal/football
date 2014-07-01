bf77094fd08113ba2d470890279fc204c31fbf78
Abhi Agarwal <me@abhiagarwal.com>
2014-07-01 17:06:18 +0800

    Updating and completing before evaluation

--- evaluate.py
+++ evaluate.py
@@ -1,6 +1,37 @@
 import json, unicodedata
 
+# Finds all games for a set of countries
+def findAllGamesForGroup(matches, country):
+    toReturn = []
+    for i in matches:
+        if i['Against'] == country:
+            toReturn.append(i)
+    return toReturn
+
+# Checks how many countries there are that the a country has played
+# more than just once
+def findNumberOfMultipleGames(matches):
+    toCheck = {}
+    toReturn = 0
+    for i in matches:
+        if i['Against'] in toCheck:
+            toReturn += 1
+        else:
+            toCheck[(i['Against'])] = 1
+    return toReturn
+
+# Checks if a country has played against another
+def playedAgainst(matches, country):
+    if findAllGamesForGroup(matches, country):
+        return True
+    else:
+        return False
+
+# Evaluation to see which country would win given a set of them
 def evaluate(year, matches, current):
+    # print findAllGamesForGroup(matches['Germany'], u'Sweden')
+    # print findNumberOfMultipleGames(matches['Germany'])
+    print playedAgainst(matches['Germany'], u'United States')
     pass
 
 if __name__ == '__main__':
@@ -16,9 +47,11 @@ if __name__ == '__main__':
         allMatches = json.load(data_file)
     for i in winners:
         finalScore = u"{0}".format(i['Final Score'])
-        yearWinner[i['Year']] = i
+        yearWinner[i['Year']] = {'Winner': i['Winner'], 'Runner-up': i['Runners-up'], 'Final Score': finalScore}
     for i in allMatches:
         if i['Year'] != 2014:
+            # Old technique to use a tuple to group the teams together
+            # in the dictionary, but lets not go this way.
             '''
             if (i['Team 1'], i['Team 2']) in matches or (i['Team 2'], i['Team 1']) in matches:
                 if (i['Team 2'], i['Team 1']) in matches:
@@ -33,18 +66,21 @@ if __name__ == '__main__':
                 matches[(i['Team 1'], i['Team 2'])] = []
                 matches[(i['Team 1'], i['Team 2'])].append(i)
             '''
+            # Evaluating the result
+            # Grouping each country together, and cutting out some details
+            # we don't need for evaluation.
+            m = {'Result': i['Result'], 'Against': i['Team 2'], 'Year': i['Year']}
+            n = {'Result': i['Result'], 'Against': i['Team 1'], 'Year': i['Year']}
             if (i['Team 1']) in matches:
-                matches[(i['Team 1'])].append(i)
+                matches[(i['Team 1'])].append(m)
             else:
                 matches[(i['Team 1'])] = []
-                matches[(i['Team 1'])].append(i)
-
+                matches[(i['Team 1'])].append(m)
             if (i['Team 2']) in matches:
-                matches[(i['Team 2'])].append(i)
+                matches[(i['Team 2'])].append(n)
             else:
                 matches[(i['Team 2'])] = []
-                matches[(i['Team 2'])].append(i)
+                matches[(i['Team 2'])].append(n)
         else:
             current.append(i)
-    print (matches['Germany'])
     evaluate(yearWinner, matches, current)
